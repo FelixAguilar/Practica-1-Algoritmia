42ad995290763e3c86f607b8ca7fec4619940abf
Felix Aguilar Ferrer <aguilarferrerfelix@gmail.com>
2019-11-10 19:23:21 +0100

    plantilla para las operaciones

---
+++ Practica 1/src/Main/Operations.java
@@ -0,0 +1,36 @@
+package Main;
+
+import Lists.Lista_Cursos;
+import Lists.Lista_Estudiantes;
+
+/**
+ * Contiene todos los metodos a ejecutar desde la interfaz grafica.
+ * 
+ * @author Felix Aguilar Ferrer, Adrian Bennasar Polzin, Alvaro Bueno Lopez.
+ */
+public class Operations {
+   
+    //Lista de los cursos.
+    public Lista_Cursos cursos = new Lista_Cursos();
+    
+    //Lista de los estudiantes.
+    public Lista_Estudiantes estudiantes = new Lista_Estudiantes();
+    
+    //Dar de alta un curso, tambien dar de alta todas sus asignaturas de forma ordenada ascendiente de su identificador o nombre.
+    
+    
+    //Matricular un estudiante de una asignatura dada, los estudiantes tambien han de estar ordenados por nombre en la assignatura.
+    
+    
+    //Dar de baja un curso, dando de baja tambien las asignaturas y la participacion de los estudiantes en esta.
+    
+    
+    //Dar de baja una asignatura, dando de baja tambien la participacion de los estudiantes en la misma.
+    
+    
+    //Dada una asignatura, indicar a que curso pertenece con el tipo y su especialidad del curso juntamente con los estudiantes matriculados en la misma.
+    
+    
+    //Dado un estudiante, indicar a que asignaturas con su informacion y a que curso pertenenen de cada asignatura.
+    
+}
